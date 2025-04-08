e19df964591638e368de3faf649f8af0b52c0897
Berkat Tua Siallagan <berkatsiallagan201105@gmail.com>
2025-04-08 15:29:43 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata Berkat Tua Siallagan</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+</head>
+<body>
+    <div class="card">
+    <img src="{{ asset('img/berkat.jpg') }}" alt="Foto Profile" class="profile-img">
+        <h2>Berkat Tua Siallagan</h2>
+        <div class="biodata-list">
+            <div class="item">
+                <div class="label">Tempat, Tanggal Lahir</div>
+                <div class="value">Batam, 11 November 2005</div>
+            </div>
+            <div class="item">
+                <div class="label">Jenis Kelamin</div>
+                <div class="value">Laki-laki</div>
+            </div>
+            <div class="item">
+                <div class="label">Alamat</div>
+                <div class="value">Perumahan Permata Asri 2 Blok PD No 11</div>
+            </div>
+            <div class="item">
+                <div class="label">Email</div>
+                <div class="value">berkatsiallagan201105@gmail.com</div>
+            </div>
+            <div class="item">
+                <div class="label">No. HP</div>
+                <div class="value">0822-8471-0929</div>
+            </div>
+            <div class="item">
+                <div class="label">Hobi</div>
+                <div class="value">Main Fanny</div>
+            </div>
+        </div>
+        <h5>Https://berkat.my.id</h5>
+    </div>
+</body>
+</html>
